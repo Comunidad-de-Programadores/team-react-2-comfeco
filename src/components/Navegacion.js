94aea3ad7b8ffc43ff9ffb084748c882013e6908
Jesusreyes <chuchin43@outlook.com>
2021-02-11 08:16:06 -0600

    Inicio del proyecto

---
+++ src/components/Navegacion.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navegacion = () => {
+    return (
+        <>
+            <nav>
+                Componente Navegacion
+            </nav>
+        </>
+    )
+}
+
+export default Navegacion
