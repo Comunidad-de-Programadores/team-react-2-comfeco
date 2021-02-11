52776dc3f2711f56c95f00637f385dfad8ce3e40
Mauricio Cartagena <mc973220@gmail.com>
2021-02-11 18:46:59 -0400

    se realizo el screen de recuperacion de contraseña

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Recuperar } from './components/auth/Recuperar'
 import Navegacion from './components/Navegacion'
 
 const App = () => {
     return (
         <div>
             <Navegacion/>
-
+            <Recuperar/>
         </div>
     )
 }
