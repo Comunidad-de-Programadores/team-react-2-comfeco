920eb6735a8dc77ab8d42ec6ae9b1c8a03315fef
Jesusreyes <chuchin43@outlook.com>
2021-02-18 18:35:40 -0600

    Contador sencillo agregado

---
+++ src/components/HomePage/Clock.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+
+const Contador = () => {
+
+    const endTime = new Date("February 10, 2022 17:25:00");
+
+    const [time, setTime] = useState({
+        dias: 0,
+        hrs: 0,
+        min: 0,
+        seg: 0,
+
+    });
+
+    const { dias, hrs, min, seg } = time;
+
+
+    useEffect(() => {
+
+        const total = endTime - new Date();
+        const tiempoTranscurrido = setTimeout(() => {
+
+            setTime({
+                dias: Math.floor(total / (1000 * 60 * 60 * 24)),
+                hrs: Math.floor((total / (1000 * 60 * 60)) % 24),
+                min: Math.floor((total / 1000 / 60) % 60),
+                seg: Math.floor((total / 1000) % 60),
+            });
+        }, 1000);
+
+        return () => {
+            clearTimeout(tiempoTranscurrido)
+        }
+    }, [seg])
+
+
+    return (
+        <>
+
+            <div className="clock">
+                <div className="clock__content">
+
+                    <div className="clock__days">
+                        <span >{dias}</span>
+                        <div className="clock__smalltext">Dias</div>
+                    </div>
+                    <div className="clock__hours">
+                        <span >{hrs}</span>
+                        <div className="clock__smalltext">Horas</div>
+                    </div>
+                    <div className="clock__minutes">
+                        <span >{min}</span>
+                        <div className="clock__smalltext">Minutos</div>
+                    </div>
+                    <div className="clock__seconds">
+                        <span >{seg}</span>
+                        <div className="clock__smalltext">Segundos</div>
+                    </div>
+
+                    
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contador
