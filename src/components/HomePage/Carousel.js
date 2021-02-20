4f7ab2441c7356c65b8ebbf701d847bbbb06de4d
Jesusreyes <chuchin43@outlook.com>
2021-02-20 14:43:54 -0600

    Carousel, con imagenes

--- src/components/HomePage/Carousel.js
+++ src/components/HomePage/Carousel.js
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getLideres } from './dataCarousel/getLideres';
 
 export const Carousel = () => {
 
-    //  <-------
+
+
+    const lideres = getLideres();
+    const [animation, setAnimation] = useState({
+        'ant': true,
+    });
+
+
+
+    const [slide, setSlide] = useState([
+        lideres[0],
+        lideres[1],
+        lideres[2],
+    ]);
+
+    useEffect(() => {
+       
+
+        console.log(`en el efecto ${animation.ant}`)
+        let timer = setTimeout(() => {
+            setAnimation({
+                ant: true
+            });
+        }, 500)
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [slide])
+
 
     const handleChangeColor = () => {
-
-        const cartas = [...document.querySelectorAll('.fs-block')];
-
-        const colores = cartas.map(c => {
-            return window.getComputedStyle(c).getPropertyValue('background-color');
+        setAnimation({
+            ant: false
+        });
+        setSlide(prev => {
+            
+            const last = lideres.map(lider => {
+                return lider.id;
+            }).lastIndexOf(`${prev[prev.length - 1].id}`);
+            
+            prev.shift();
+            
+            if (last >= 19) {
+                return [
+                    ...prev,
+                    lideres[0]
+                ]
+            }
+            return [
+                ...prev,
+                lideres[last + 1]
+            ]
         });
 
-        console.log(`colores: ${colores}`)
-        cartas.forEach((c, i) => {
-            (i == colores.length - 1)
-                ? c.style.setProperty(`--color-${i + 1}`, `${colores[0]}`)
-                : c.style.setProperty(`--color-${i + 1}`, `${colores[i + 1]}`)
-        });
     }
 
 
@@ -26,26 +66,24 @@ export const Carousel = () => {
             <section className="fs-container">
                 <div className="fs-wrapper">
                     <div className="fs-slider">
-                        <div className="fs-block">
-                            <figure>
+                        {
+                            slide.map((lider, i) => (
+                                <div className={`fs-block transition-${i}`} key={lider.id}>
+                                    <figure className={ 
+                                            animation.ant ? 'fs-transition': 'fs-remove'
+                                    }>
+                                        <img src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F744f9499-dbb7-4a55-92b2-1f0781448bb9%2F1_prCweEqN9wGLx19kngaW2A.jpeg?table=block&id=31a42319-8352-4cba-935e-f323bf41c7a5&width=2720&userId=&cache=v2" alt="image01" />
+                                        <figcaption className={ 
+                                            animation.ant ? 'fs-transition': 'fs-remove'
+                                        }>
+                                            <h3>{lider.name}</h3>
+                                            <p>{lider.team}</p>
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                            ))
+                        }
 
-                            </figure>
-                        </div>
-                        <div className="fs-block">
-                            <figure>
-
-                            </figure>
-                        </div>
-                        <div className="fs-block">
-                            <figure>
-
-                            </figure>
-                        </div>
-                        <div className="fs-temp">
-                            <figure>
-
-                            </figure>
-                        </div>
                     </div>
 
                     <nav className="fs-navigation">
@@ -58,3 +96,16 @@ export const Carousel = () => {
         </>
     )
 }
+
+
+{/* <div className="fs-block">
+</div>
+<div className="fs-block">
+    
+</div>
+<div className="fs-block">
+    
+</div>
+<div className="fs-temp">
+    
+</div> */}
--- src/components/HomePage/dataCarousel/getLideres.js
+++ src/components/HomePage/dataCarousel/getLideres.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { lideres } from './data';
 
 
-export const getLideres = (team) => {
+export const getLideres = () => {
     
-    const validTeams = ['angular', 'react', 'vue', 'svelte'];
+    // const validTeams = ['angular', 'react', 'vue', 'svelte'];
 
-    if(! validTeams.includes(team)){
-        throw new Error(`Publisher "${ publisher }" no es correcto`);
-    }
+    // if(! validTeams.includes(team)){
+    //     throw new Error(`Publisher "${ team }" no es correcto`);
+    // }
 
-    return lideres.filter(lider => lider.team === team);
+    return lideres;
 
 
 
