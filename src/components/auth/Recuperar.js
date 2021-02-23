43338bf8b3a8664c3479f53ab351c9e4a5c72943
Jesusreyes <chuchin43@outlook.com>
2021-02-23 14:38:11 -0600

    Recuperar-Page, estilos agregados

--- src/components/auth/Recuperar.js
+++ src/components/auth/Recuperar.js
@@ -2,46 +2,74 @@ import React from 'react';
 import validator from 'validator';
 import { useForm } from 'react-hook-form';
 import Navegacion from '../Navegacion';
+import { useState } from 'react';
 
 export const Recuperar = () => {
+
+   const [alerta, setAlerta] = useState({
+      alert: false, 
+      msg: 'Este campo es requerido'
+   });
+
+
+
    const { register, handleSubmit, watch, errors } = useForm();
    const onSubmit = (data) => {
+      console.log(register.length)
       console.log(data);
       console.log(validator.isEmail(data.email));
+      if(!validator.isEmail(data.email)){
+         console.log("alerta activada")
+         setAlerta({
+            alert: true, 
+            msg: 'Ingresar un correo valido'
+         });
+      }
+
    };
-   console.log(watch('example')); // watch input value by passing the name of it
+
+   const handleCorreo = (e) => {
+
+      if(e.target.value.length > 0 ){
+         setAlerta({
+            alert: false,
+            msg: 'Este campo es requerido',
+         });
+     
+      }
+      
+   }
 
    return (
       <Navegacion Userdata={{ email: '', isLogin: false }}>
          {
-            <div className="container">
-               <div className="sub-container">
-                  <h3 className="title">Reasignar contraseña</h3>
-                  <p className="paragraph">
+            <div className="recuperar">
+               <div className="recuperar__container">
+                  <h3 className="recuperar__title">Reasignar contraseña</h3>
+                  <p className="recuperar__paragraph">
                      Te enviaremos un correo electrónico con un enlace privado
                      para que reasignes tu contraseña este enlace será valido
                      por una hora
                   </p>
                   <form onSubmit={handleSubmit(onSubmit)}>
-                     <br />
-                     <input
-                        className="input-container"
-                        placeholder="Correo electrónico"
-                        name="email"
-                        ref={register({ required: true })}
-                     />
-                     <br />
-                     <br />
-                     {errors.email && (
-                        <div style={{ marginBottom: 7 }}>
-                           <span>Este campo es requerido</span>
-                        </div>
-                     )}
-                     <div className="colum">
-                        <button className="btn-send" type="submit">
+                     <label className="recuperar__correo">
+                        <input
+                           placeholder=" "
+                           name="email"
+                           autoComplete="off"
+                           onChange={handleCorreo}
+                           ref={register({ required: true })}
+                        />
+                        <span className={alerta.alert ? '--active': ''}>Correo Electronico</span>
+                     </label>
+                    
+                     <div className= {`recuperar__alerta${alerta.alert ? '--active': ''}`}>
+                        <span>{alerta.msg}</span>
+                     </div>
+                     <div className="recuperar__enlace">
+                        <button className="btn btn-blue" type="submit">
                            Enviar Enlace
                         </button>
-                        <button className="btn-login">Iniciar Sessión</button>
                      </div>
                   </form>
                </div>
