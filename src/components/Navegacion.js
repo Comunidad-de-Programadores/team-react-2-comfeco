02442ee53ece387de8c64e9ab746d282eab90d0b
Darkzalo <gonza1810@gmail.com>
2021-02-25 14:23:28 -0300

    Agregando UserRamdom, Bell-icons. Close #18

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+
 import { isLogin, logout } from '../shared/login';
+import apiUser from '../shared/api/userRamdom';
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import logo from '../assets/logo3.png';
 
 const Navegacion = ({ children }) => {
    const [isLoginState, setisLogin] = useState(false);
-   const [user, setUser] = useState('');
+   const [user, setUser] = useState([]);
 
    const history = useHistory();
 
    useEffect(() => {
       const Login = isLogin();
-      if (Login) setUser(localStorage.getItem('email'));
+      if (Login) {
+         apiUser.getUser().then((data) => {
+            setUser(data);
+            console.log(data);
+         });
+      }
 
       setisLogin(Login);
    }, []);
@@ -28,11 +38,6 @@ const Navegacion = ({ children }) => {
       logout();
       setisLogin(false);
       history.push('/');
-   };
-
-   const clickHome = (e) => {
-      e.preventDefault();
-      history.push('/home');
    };
 
    return (
@@ -78,14 +83,29 @@ const Navegacion = ({ children }) => {
                         Creadores de Contenidos
                      </Link>
                   </li>
-                  <li className="navegacion__nav-bar--item">
-                     <span>{user}</span>
-                  </li>
-                  <li className="navegacion__nav-bar--item">
-                     <button className="btn btn-purple" onClick={handleLogout}>
-                        Logout
+                  <li className="navegacion__nav-bar--item user">
+                     <span>{<FontAwesomeIcon icon={faBell} />}</span>
+
+                     {user?.map((item) => {
+                        return (
+                           <span>
+                              {' '}
+                              <img
+                                 src={item.picture.thumbnail}
+                                 alt="photo"
+                              />{' '}
+                              {item.email}
+                           </span>
+                        );
+                        // <img src={item.picture.medium} alt="photo"/>
+                     })}
+                     {/* <span>{user}</span> */}
+                     <button onClick={handleLogout}>
+                        {<FontAwesomeIcon icon={faTimes} />}
+                        {/* <i class="far fa-window-close"></i> */}
                      </button>
                   </li>
+                  <li className="navegacion__nav-bar--item"></li>
                </ul>
             </div>
          )}
