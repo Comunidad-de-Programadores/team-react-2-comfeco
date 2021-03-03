10f1fa24ce8da39ffaba6b2b66bc97b7a2962a11
Mauricio Cartagena <mc973220@gmail.com>
2021-03-03 05:06:26 -0400

    se agrego algunos estilos al formulario de perfil

--- src/components/HomePage/perfil/ScreenPerfil.js
+++ src/components/HomePage/perfil/ScreenPerfil.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 
 export const ScreenPerfil = () => {
     return (
@@ -8,77 +12,80 @@ export const ScreenPerfil = () => {
                     <form>
                         <div className="grid-container" >
                             <div className="grid-item-center" >
-                                <label>Editar Perfil</label>
+                                <label className="label-title">Editar Perfil</label>
                                 <img src="http://site-834781.mozfiles.com/files/834781/arbol.jpg" ></img>
                             </div>
                         </div>
 
                         <div className="grid-container-colum-2" >
                             <div className="grid-item" >
-                                <label>Nick</label>
-                                <input type="text" placeholder="Nick de usuario" />
+                                <label className="label" >Nick</label>
+                                <input className="gg-bound-control-input" type="text" placeholder="Nick de usuario" />
                             </div>
                             <div className="grid-item" >
-                            <label >Correo</label>
-                                <input type="email" placeholder="example@gmail.com" />
+                            <label className="label">Correo</label>
+                                <input className="gg-bound-control-input" type="email" placeholder="example@gmail.com" />
                             </div>
                         </div>
                         <div className="grid-container-3" >
                             <div className="grid-item" >
-                                <label>Genero</label>
-                                <input type="text" placeholder="Nick de usuario" />
+                                <label className="label">Genero</label>
+                                <select className="gg-bound-control-input" type="text" name="genero" id="genero" >
+                                    <option value="femenino">Femenino</option>
+                                    <option value="masculino">Masculino</option>
+                                </select>
                             </div>
                             <div className="grid-item" >
-                                <label >Fecha Nacimiento</label>
-                                <input type="email" placeholder="example@gmail.com" />
+                                <label className="label" >Fecha Nacimiento</label>
+                                <input className="gg-bound-control-input" type="email" placeholder="example@gmail.com" />
                             </div>
                             <div className="grid-item" >
-                                <label >Pais</label>
-                                <input type="email" placeholder="example@gmail.com" />
+                                <label className="label" >Pais</label>
+                                <input className="gg-bound-control-input" type="text" placeholder="Estados unidos" />
                             </div>
                         </div>
                         <div className="grid-container-colum-2" >
                             <div className="grid-item" >
-                                <label>Contraseña</label>
-                                <input type="text" placeholder="Nick de usuario" />
+                                <label className="label">Contraseña</label>
+                                <input className="gg-bound-control-input" type="password" placeholder="Contraseña" />
                             </div>
                             <div className="grid-item" >
-                            <label >Repita contraseña</label>
-                                <input type="email" placeholder="example@gmail.com" />
-                            </div>
-                        </div>
-                        <div className="grid-container-colum-4" >
-                            <div className="grid-item" >
-                                <label>facebook</label>
-                            </div>
-                            <div className="grid-item" >
-                                <input type="email" placeholder="example@gmail.com" />
-                            </div>
-                            <div className="grid-item" >
-                                <label>github</label>
-                            </div>
-                            <div className="grid-item" >
-                                <input type="email" placeholder="example@gmail.com" />
+                            <label className="label">Repita contraseña</label>
+                                <input className="gg-bound-control-input" type="password" placeholder="Repita contraseña" />
                             </div>
                         </div>
                         <div className="grid-container-colum-4" >
                             <div className="grid-item" >
-                                <label>Linkendin</label>
+                                <FontAwesomeIcon icon={ faFacebook } />
                             </div>
                             <div className="grid-item" >
-                                <input type="email" placeholder="example@gmail.com" />
+                                <input className="gg-bound-control-input" type="email" placeholder="example@gmail.com" />
                             </div>
                             <div className="grid-item" >
-                                <label>Twiter</label>
+                                <FontAwesomeIcon icon={ faGithub } />
                             </div>
                             <div className="grid-item" >
-                                <input type="email" placeholder="example@gmail.com" />
+                                <input className="gg-bound-control-input" type="email" placeholder="example@gmail.com" />
+                            </div>
+                        </div>
+                        <div className="grid-container-colum-4" >
+                            <div className="grid-item" >
+                                <FontAwesomeIcon icon={ faLinkedin } />
+                            </div>
+                            <div className="grid-item" >
+                                <input className="gg-bound-control-input" type="email" placeholder="example@gmail.com" />
+                            </div>
+                            <div className="grid-item" >
+                                <FontAwesomeIcon icon={ faTwitter } />
+                            </div>
+                            <div className="grid-item" >
+                                <input className="gg-bound-control-input" type="email" placeholder="example@gmail.com" />
                             </div>
                         </div>
                         <div className="grid-container" >
                             <div className="grid-item" >
-                                <label>Biografia</label>
-                                <textarea type="text" ></textarea>
+                                <label className="label">Biografia</label>
+                                <textarea className="gg-bound-control-input" type="text" ></textarea>
                             </div>
                         </div>
                         <div className="grid-container" >
