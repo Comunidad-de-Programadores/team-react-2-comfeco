c4d22400e02050080fd739e57c0cf80ad01b9098
Bryan Manzano <bryanmmr@gmail.com>
2021-03-08 00:27:56 -0500

    profile image

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -66,3 +66,4 @@ export const registro = (data) => {
       });
 }
 
+
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,6 +3,8 @@ import { useHistory } from 'react-router';
 import { Actividades } from './Actividades';
 import { BannerInsignias } from './BannerInsignias';
 import BannerButtons from './BannerButtons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Profile = () => {
    const history = useHistory();
@@ -10,11 +12,25 @@ export const Profile = () => {
    const handleEditProfile = () => {
       history.push('/home/profile');
    };
+   console.log(faExternalLinkAlt);
    return (
       <div className="profile">
          <BannerButtons />
          <div>
-            Componente perfil Izquierda
+            <div className='profile-container'>
+               <div className='profile-image'>
+                  <img></img>
+               </div>
+               <div className='profile-name'>Usuario
+                  <div className='profile-edit' onClick={handleEditProfile}>
+                     <FontAwesomeIcon icon={faExternalLinkAlt} />
+                  </div>
+               </div>
+               <div className='profile-title'>Front End Dev</div>
+               <div className='profile-content'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente facilis iure veniam dolor, ipsam cupiditate at quae ex, non molestias quas eveniet dignissimos! Dolorem eum totam possimus praesentium vitae aliquam!
+               </div>
+            </div>
             <a onClick={handleEditProfile}> Editar perfil </a>
             <BannerInsignias />
          </div>
